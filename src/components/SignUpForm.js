4e2228ac2360f900126725f795ac28e0eed6d0d7
Aidan Shields <G00370587@atu.ie>
2022-12-20 14:43:08 +0000

    Project setup and layout

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const Form = ({ handleClose }) => {
+  // create state variables for each input
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password);
+    handleClose();
+  };
+
+  return (
+    <form 
+    style={{ width: '300px' }}
+    onSubmit={handleSubmit}
+    >
+      <TextField
+      sx={{ display: 'flex',
+        margin: '3%'}}
+        label="First Name"
+        variant="filled"
+        required
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <TextField
+      sx={{
+        display: 'flex',
+        margin: '3%'}}
+        label="Last Name"
+        variant="filled"
+        required
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <TextField
+      sx={{
+        display: 'flex',
+        margin: '3%'}}
+        label="Email"
+        variant="filled"
+        type="email"
+        required
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <TextField
+      sx={{
+        display: 'flex',
+        margin: '3%'}}
+        label="Password"
+        variant="filled"
+        type="password"
+        required
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <div
+      style={{ display: "flex", justifyContent:"space-evenly" }}
+       >
+        <Button 
+        sx={{
+            margin: '5%',
+        }}
+        variant="contained" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+         sx={{
+            margin: '5%',
+        }}
+         type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
