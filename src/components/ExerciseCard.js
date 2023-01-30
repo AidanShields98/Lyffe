d9e349f098b7ef1c25d91f77d9db65648a5c46a7
Aidan Shields <g00370587@atu.ie>
2023-01-30 11:22:10 +0000

    added in some work into api page

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -9,12 +9,12 @@ const ExerciseCard = ({ exercise }) => (
       <Button sx={{ ml: '21px', color: '#fff', background: '#262424', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.bodyPart}
       </Button>
-      <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+      <Button sx={{ ml: '21px', color: '#fff', background: '#262424', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.target}
       </Button>
 
     </Stack>
-    <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
+    <Typography ml="21px" color="#000" textAlign={ "center"} fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
       {exercise.name}
     </Typography>
   </Link>
