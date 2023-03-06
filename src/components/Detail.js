ac15c5196e9e8501986888d10ab9c5a17a93236f
Aidan Shields <g00370587@atu.ie>
2023-03-06 18:20:29 +0000

    RP-15 Added images for each icon, added ExerciseDeatil page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { Typography, Stack, Button } from '@mui/material';
 
-import BodyPartImage from '../assets/icons/body-part.png';
-import TargetImage from '../assets/icons/target.png';
-import EquipmentImage from '../assets/icons/equipment.png';
+import Target from '../assets/icons/Target.png';
+import Person from '../assets/icons/person.png';
+import Equipment from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
   const extraDetail = [
     {
-      icon: BodyPartImage,
+      icon: Person,
       name: bodyPart,
     },
     {
-      icon: TargetImage,
+      icon: Target,
       name: target,
     },
     {
-      icon: EquipmentImage,
+      icon: Equipment,
       name: equipment,
     },
   ];
 
   return (
-    <Stack  gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', justifyContent:'center', displayP: 'flex' }}>
+    <Stack  display={ 'flex'} alignItems="center" gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', justifyContent:'center', textAlign: 'center' }}>
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
-      <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+      <Stack display={ 'flex'} sx={{ gap: { lg: '12%', xs: '5%' } }}>
         <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
           {name}
         </Typography>
         {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
-            <Button sx={{ background: '#FFF2DB', borderRadius: '50%', width: '100px', height: '100px' }}>
+            <Button sx={{ borderRadius: '50%', width: '100px', height: '100px' }}>
               <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }} />
             </Button>
             <Typography textTransform="capitalize" sx={{ fontSize: { lg: '30px', xs: '20px' } }}>
