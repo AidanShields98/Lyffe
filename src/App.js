d57188ced2e5a044f85f10f208a7a0757acf8a42
AIDAN SHIELDS <G00370587@atu.ie>
2023-03-10 13:33:38 +0000

    RP-13 logout component built no functionality

--- src/App.js
+++ src/App.js
@@ -1,58 +1,40 @@
-import React from 'react';
-import BottomNavigation from './components/BottomNavigation';
-import AppBar from '@mui/material/AppBar';
-import { useState } from 'react';
-import SignUpDialog from './components/SignUpDialog';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import LogInDialog from './components/LoginDialog'
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import DropdownMenu from './components/DropdownMenu'
-import {Home} from './pages/Home';
-import Exercises from './pages/Exercises';
-import {Workout} from './pages/Workout';
-import {User} from './pages/User';
+import React from "react";
+import BottomNavigation from "./components/BottomNavigation";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Routes, Route } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import IconButton from "@mui/material/IconButton";
+import "./App.css";
+import { Home } from "./pages/Home";
+import Exercises from "./pages/Exercises";
+import { Workout } from "./pages/Workout";
+import { User } from "./pages/User";
 
 function App() {
-
-  const [signup, setSignUp] = useState(false);
-  const [login, setLogin] = useState(false);
-
-
-  const handleOpen = () => {
-    setSignUp(true);
-  };
-
-  const handleClose = () => {
-    setSignUp(false);
-  };
-
-  const handleOpenLogin = () => {
-    setLogin(true);
-  };
-
-  const handleCloseLogin = () => {
-    setLogin(false);
-  };
-
-
   return (
     <div>
-      <AppBar sx={{ display: 'flex', textAlign: 'center', xs: 'none', md: 'flex'}}>
-        <Toolbar> 
-           {/* <Button sx={{  width: '10%', color:'white', fontSize: '2vh', marginBottom:'.5%'  }} onClick={handleOpen}>Signup</Button> */}
-           <Typography  component="div" sx={{ flexGrow: 1, textAlign: 'center', fontSize: '4vh', marginBottom:' .5%' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            sx={{
+              textAlign: "center",
+              fontSize: "4vh",
+              flex: 1,
+            }}
+          >
             Lyffe
           </Typography>
-          {/* <Button sx={{  width: '10%', color:'white', fontSize: '2vh', marginBottom:'.5%' }} onClick={handleOpenLogin}>Login</Button> */}
-        <DropdownMenu  sx={{ alignItems: 'center', justifyContent: 'center' }}  />
-        </Toolbar>     
-      </AppBar>
-      <SignUpDialog open={signup} handleClose={handleClose} />
-      <LogInDialog open={login} handleClose={handleCloseLogin} /> 
 
-      
+          <div>
+            <IconButton aria-label="delete" sx={{ color: "#fff" }} >
+              <LogoutIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercises" element={<Exercises />} />
