5af6b0893a6e7551b2cfd83ea508140c26230af5
AIDAN SHIELDS <G00370587@atu.ie>
2023-03-10 16:25:30 +0000

    RP-19 added form and page for signup and modified some css across the app

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -36,25 +36,22 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   };
 
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+    <Stack className="search-stack">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
        Search for any<br /> Exerise or Bodypart!
       </Typography>
-      <Box position="relative" mb="72px">
+      <Box className="search-box">
         <TextField
-          height="76px"
-          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          className="search-text"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }}}
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#1976d2', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+        <Button className="search-btn" sx={{ bgcolor: '#1976d2', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
           Search
         </Button>
-      </Box>
-      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-        {/* <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} /> */}
       </Box>
     </Stack>
   );
