9db4f9a8578cc12abea68b140da7ea08ef41b1e5
Aidan Shields <G00370587@atu.ie>
2023-03-26 23:05:47 +0100

    RP-35 need to fix background as its not displaying, learned ngrok is needed at the minute for viewing on other devices, need to create sign up button to navigate to auth0 sign up

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,14 @@
 import { Button } from "@material-ui/core";
+import { Image } from "@mui/icons-material";
 import LoginButton from "../components/LoginButton";
+import gymImage from "../assets/imgs/gym.jpg";
 
 export function LandingPage() {
-
   return (
     <div className="landing-root">
+        <Image src={gymImage} />
       <div className="landing-buttonContainer">
-        <LoginButton className="landing-button"/>
+        <LoginButton className="landing-button" />
         <Button className="landing-button" variant="outlined" color="primary">
           Sign Up
         </Button>
